5379123a2af0a8731abac89f9e538ba5262b3615
Satoru Takahashi <fugasat@gmail.com>
2016-12-12 12:16:18 +0900

    風景の特徴がないデータに「特徴なし」という特徴を追加

--- photo_analyzer.py
+++ photo_analyzer.py
@@ -3,7 +3,17 @@ import pandas as pd
 
 def read_csv():
     df = pd.read_csv("data/instagram_data.csv")
+
+    # nanを0に変換
     df.ix[:, "c":"踏切"] = df.ix[:, "c":"踏切"].fillna(0)
+
+    # 風景の特徴が無いデータを「特徴なし」とする
+    no_feature = []
+    for i, row in df.iterrows():
+        no_feature.append(row["森林" : "踏切"].sum())
+
+    df["特徴なし"] = no_feature
+
     return df
 
 if __name__ == "__main__":
