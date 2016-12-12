d5bf70c51eb05eebf33a7b7fc3b59c6b28403a88
Satoru Takahashi <fugasat@gmail.com>
2016-12-13 01:29:25 +0900

    車両形式を取得

--- photo_analyzer.py
+++ photo_analyzer.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 
 def read_csv():
@@ -7,11 +8,28 @@ def read_csv():
     # nanを0に変換
     df.ix[:, "c":"踏切"] = df.ix[:, "c":"踏切"].fillna(0)
 
-    # 風景の特徴が無いデータを「特徴なし」とする
+    # 登録された車両形式を抽出
+    type_df = df.iloc[:, 17:23]
+    type_array = type_df.as_matrix().flatten()
+    type_array = type_array.astype('str')
+    type_array = np.unique(type_array)
+    type_dic = {}
+    for item in type_array:
+        type_dic[item] = []
+
     no_feature = []
     for i, row in df.iterrows():
+        # 風景の特徴が無いデータを「特徴なし」とする
         no_feature.append(row["森林" : "踏切"].sum())
+        # 特定の車両が存在するかどうかチェック
+        types = row[17:23].astype('str')
+        for item in type_array:
+            exists = item in types.as_matrix()
+            array = type_dic[item]
+            array.append(int(exists))
+            type_dic[item] = array
 
+    # 風景の特徴が無いデータのカラムを追加
     df["特徴なし"] = no_feature
 
     return df
