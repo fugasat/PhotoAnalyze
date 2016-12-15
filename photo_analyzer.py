5dda6879a64cf4d95d3a0e101b1d9bf25a0cf244
Satoru Takahashi <fugasat@gmail.com>
2016-12-15 15:26:17 +0900

    各種分類器を評価

--- photo_analyzer.py
+++ photo_analyzer.py
@@ -1,6 +1,15 @@
+import random
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn import svm
+from sklearn.cross_decomposition import PLSRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, accuracy_score
+from sklearn.metrics import precision_recall_curve
+from sklearn.cross_validation import KFold
 
 
 def read_csv():
@@ -71,10 +80,118 @@ def good_data(data, y_lr_e):
     return data.ix[y_mask]
 
 
+def create_dataset(data, y_lr_e, num_train=30):
+    y_lr_e_std = y_lr_e.std() #* 0
+    y_mask = y_lr_e >= y_lr_e_std
+
+    data = data.drop("日付", axis=1)
+    data = data.drop("f", axis=1)
+    data = data.drop("c", axis=1)
+
+    pos_data = data.ix[y_mask]
+    neg_data = data.ix[~y_mask]
+
+    pos_label = np.ones(pos_data.shape[0])
+    neg_label = np.zeros(neg_data.shape[0])
+    pos_label_e = y_lr_e[y_mask]
+    neg_label_e = y_lr_e[~y_mask]
+
+    pos_index = np.arange(len(pos_data))
+    random.shuffle(pos_index)
+    pos_train_index = pos_index[:num_train]
+    pos_test_index = pos_index[num_train:]
+
+    neg_index = np.arange(len(neg_data))
+    random.shuffle(neg_index)
+    neg_train_index = neg_index[:num_train]
+    neg_test_index = neg_index[num_train:]
+
+    x = data.as_matrix()
+    y = np.array([1 if y >= y_lr_e_std else 0 for y in y_lr_e.as_matrix()])
+    y_e = y_lr_e.as_matrix()
+    train_x = pd.concat([pos_data.iloc[pos_train_index, :], neg_data.iloc[neg_train_index, :]]).as_matrix()
+    train_y = np.hstack((pos_label[pos_train_index], neg_label[neg_train_index]))
+    train_y_e = pd.concat([pos_label_e.iloc[pos_train_index], neg_label_e.iloc[neg_train_index]]).as_matrix()
+    test_x = pd.concat([pos_data.iloc[pos_test_index, :], neg_data.iloc[neg_test_index, :]]).as_matrix()
+    test_y = np.hstack((pos_label[pos_test_index], neg_label[neg_test_index]))
+    test_y_e = pd.concat([pos_label_e.iloc[pos_test_index], neg_label_e.iloc[neg_test_index]]).as_matrix()
+
+    print("pos:" + str(len(pos_data)))
+    print(" -train:" + str(len(pos_train_index)))
+    print(" -test:" + str(len(pos_test_index)))
+    print("neg:" + str(len(neg_data)))
+    print(" -train:" + str(len(neg_train_index)))
+    print(" -test:" + str(len(neg_test_index)))
+    return x, y, y_e, train_x, train_y, train_y_e, test_x, test_y, test_y_e
+
+
 def bad_data(data, y_lr_e):
     y_lr_e_std = y_lr_e.std()
     y_mask = y_lr_e >= -y_lr_e_std
     return data.ix[y_mask]
+
+
+def train_pls(train_x, train_y, test_x, test_y):
+    print("train : PLS")
+
+    # 学習
+    clf = PLSRegression(n_components=15, scale=True)
+    clf.fit(train_x, train_y_e)
+
+    # 学習結果を確認
+    test_pred = clf.predict(test_x)
+    print("test_pred" + str(test_pred))
+
+
+def train_cv(clf, x, y, train_x, train_y, test_x, test_y):
+    scores = []
+    cv = KFold(len(x), n_folds=10)
+
+    for train, test in cv:
+        x_train, y_train = x[train], y[train]
+        x_test, y_test = x[test], y[test]
+        clf.fit(x, y)
+        scores.append(clf.score(x_test, y_test))
+        #precision, recall, thresholds = precision_recall_curve(y_test, clf.predict(x_test))
+        #medium = np.argsort(scores)[len(scores) / 2]
+        #thresholds = np.hstack(([0], thresholds[medium]))
+        #idx80 = precision >= 0.8
+        #print("P=%.2f R=%.2f thresh=%.2f" % (precision[idx80][0], recall[idx80][0], thresholds[idx80][0]))
+        pass
+
+    print("Mean(scores)=%.5f Stddev(scores)=%.5f" % (np.mean(scores), np.std(scores)))
+    return clf
+
+
+def train(clf, x, y, train_x, train_y, test_x, test_y):
+    # 学習
+    clf.fit(train_x, train_y)
+
+    # 学習結果を確認
+    test_pred = clf.predict(test_x)
+    print(classification_report(test_y, test_pred))
+    print("accuracy_score = " + str(accuracy_score(test_y, test_pred)))
+    print()
+
+
+def train_svm(x, y, train_x, train_y, test_x, test_y):
+    print("train : SVM")
+    train_cv(svm.SVC(), x, y, train_x, train_y, test_x, test_y)
+
+
+def train_random_forest(x, y, train_x, train_y, test_x, test_y):
+    print("train : Random Forest")
+    train_cv(RandomForestClassifier(n_estimators=100), x, y, train_x, train_y, test_x, test_y)
+
+
+def train_kn(x, y, train_x, train_y, test_x, test_y):
+    print("train : KNeighbors")
+    train_cv(KNeighborsClassifier(n_neighbors=2), x, y, train_x, train_y, test_x, test_y)
+
+
+def train_logistic_regression(x, y, train_x, train_y, test_x, test_y):
+    print("train : LogisticRegression")
+    return train_cv(LogisticRegression(), x, y, train_x, train_y, test_x, test_y)
 
 
 if __name__ == "__main__":
@@ -100,6 +217,32 @@ if __name__ == "__main__":
     plt.plot(x, x * 0 - y_lr_e_std, color="#ff4040")
     plt.plot(x, y_lr_e)
     plt.savefig('date_e.png')
+
+    print()
+
+    # 教師データ、テストデータ生成
+    x, y, y_e, train_x, train_y, train_y_e, test_x, test_y, test_y_e = create_dataset(data, y_lr_e, 100)
+
+    # 学習
+    train_kn(x, y, train_x, train_y, test_x, test_y)
+    train_svm(x, y, train_x, train_y, test_x, test_y)
+    train_random_forest(x, y, train_x, train_y, test_x, test_y)
+    clf = train_logistic_regression(x, y, train_x, train_y, test_x, test_y)
+
+    print()
+
+    coef = clf.coef_
+    data_coef = data.drop("日付", axis=1)
+    data_coef = data_coef.drop("f", axis=1)
+    data_coef = data_coef.drop("c", axis=1)
+    col = data_coef.columns
+    index = 0
+    for c in col:
+        if coef[0][index] > 0.4:
+            print("(!)" + c + ":" + str(coef[0][index]))
+        else:
+            print("   " + c + ":" + str(coef[0][index]))
+        index = index + 1
 
     # 回帰誤差を２番目の列に挿入する
     data["回帰誤差"] = y_lr_e
